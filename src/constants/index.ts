268449097949cd64891893706501ccb668577411
netfily <83597725+netfily@users.noreply.github.com>
2021-05-04 14:44:39 +0700

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
